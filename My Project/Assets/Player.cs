7577863ad6b8f86799d1350e0a60fa6c66c00954
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-01-20 16:53:17 -0700

    Added Horizontal Movement

--- My Project/Assets/Player.cs
+++ My Project/Assets/Player.cs
@@ -15,12 +15,21 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * 0.5f;
+            transform.position += Vector3.forward * 0.1f;
         }
         
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * 0.5f;
+            transform.position += Vector3.back * 0.1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.1f;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.1f;
         }
     }
 }
